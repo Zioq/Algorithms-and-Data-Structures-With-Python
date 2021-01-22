b060c37f056dcc1a6167d2fd33d4c955207f1ddf
Zioq <jr.roberthan@gmail.com>
2021-01-22 10:36:40 -0800

    basic function concpet

--- 6.Functions/lecture_2.py
+++ 6.Functions/lecture_2.py
@@ -1,1 +1,20 @@
 # Functions - implementation step by step
+
+def get_input_from_user():
+    ''' result = input("Enter your response here -> ")
+    return result '''
+    return input("Enter your response here -> ")
+
+print("Welcome to the program, what is your name?")
+name_result = get_input_from_user()
+
+
+print("What did yoyu think of the food you ate today?")
+food_result = get_input_from_user()
+
+print("What tv show ending did you dislike most ever?")
+show_result = get_input_from_user()
+
+print(f"To summarize: Your name is {name_result.capitalize()},\
+ you ate {food_result} food today and hated the ending \
+ of {show_result.upper()}")
