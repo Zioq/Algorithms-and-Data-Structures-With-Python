4de8103c4c3773f80c75cf02cf2c1a7ef4f6c823
Zioq <jr.roberthan@gmail.com>
2021-01-28 11:20:36 -0800

    selection sort implementation

--- 9.Selection Sort/lecture_1.py
+++ 9.Selection Sort/lecture_1.py
@@ -1,1 +1,21 @@
-# Selection sort 
+# Selection Sort  Implementation
+
+def selection_sort(arr):
+    spot_marker = 0
+    while (spot_marker < len(arr)):
+        for num in range(spot_marker, len(arr)):
+            if arr[num] < arr[spot_marker]:
+                arr[num], arr[spot_marker] = arr[spot_marker], arr[num]
+        spot_marker += 1
+        print(arr)
+
+
+l = [6, 8, 1, 4, 10, 7, 8, 9, 3, 2, 5]
+selection_sort(l)
+
+# Interation step-by-step
+# Start iteration 0:    6, 8, 1, 4, 10, 7, 8, 9, 3, 2, 5
+# Start iteration 1:    1, 8, 6, 4, 10, 7, 8, 9, 3, 2, 5
+# Start iteration 2:    1, 2, 8, 6, 10, 7, 8, 9, 4, 3, 5
+# Sorted list:          1, 2, 3, 4, 5, 6, 7, 8, 8, 9, 10
+
