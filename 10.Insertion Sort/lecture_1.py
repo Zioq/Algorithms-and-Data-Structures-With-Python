54b2f0fe34f80af49cc1dbfe4c7ee16a32bfdf78
Zioq <jr.roberthan@gmail.com>
2021-01-29 10:57:47 -0800

    update insertion sort

--- 10.Insertion Sort/lecture_1.py
+++ 10.Insertion Sort/lecture_1.py
@@ -2,10 +2,10 @@
 
 def insertion_sort(arr):
     # Always set the key index as 1 in insertion sort
-    for key in range(1, len(arr)):
-        if arr[key] < arr[key-1]:
+    for key in range(1, len(arr)): # key increment process
+        if arr[key] < arr[key-1]: 
             j = key
-            while j > 0 and arr[j] < arr[j-1]:
+            while j > 0 and arr[j] < arr[j-1]: # sub-sorted array implementation
                 arr[j], arr[j-1] = arr[j-1] , arr[j]
                 j -= 1
     print(arr)
