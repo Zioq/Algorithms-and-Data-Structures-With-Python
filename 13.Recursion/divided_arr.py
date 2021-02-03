dc33f897a947c5e6104d39a8785af8a9f1657b76
Zioq <jr.roberthan@gmail.com>
2021-02-03 10:19:34 -0800

    update recursion mini project: couterdown

--- 13.Recursion/divided_arr.py
+++ 13.Recursion/divided_arr.py
@@ -4,6 +4,7 @@ def divide_arr(arr):
     # Base case
     if len(arr)< 2:
         print(f"Base condition reached with {arr[:]}")
+        # return all value in the list
         return arr[:]
     else:
         # integer division
@@ -13,6 +14,7 @@ def divide_arr(arr):
         print("Right split: ", arr[middle:])
         l1 = divide_arr(arr[:middle])
         l2 = divide_arr(arr[middle:])
+        print(l1, l2)
         # implied return None
 l = [8,6,2,5]
 divide_arr(l)
