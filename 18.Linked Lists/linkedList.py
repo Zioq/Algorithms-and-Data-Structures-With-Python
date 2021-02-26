93616784c81ade2d7c974064e95d1fee025a7ca6
Zioq <jr.roberthan@gmail.com>
2021-02-26 11:59:54 -0800

    update linked list

---
+++ 18.Linked Lists/linkedList.py
@@ -0,0 +1,55 @@
+class Node:
+
+    def __init__(self, data=None):
+        # Set the data
+        self.data = data
+        # Set the pointer
+        self.next = None
+
+    def __str__(self):
+        return f"{self.data}"
+
+
+class LinkedList:
+
+    # Initialize linked list(At the beginning, the linked list has Head with None, and Tail with None)
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def append_val(self, x):
+        ''' Add x to the end of the list '''
+        if not isinstance(x, Node):
+            x = Node(x)
+        if self.head == None:
+            self.head = x
+        else:
+            self.tail.next = x
+        self.tail = x
+
+    def __str__(self):
+        # [5->4->10->1]
+        to_print = ""
+        current = self.head
+        while current is not None:
+            to_print = to_print + str(current.data) + "->"
+            current = current.next
+        if to_print:
+            return "["+ to_print[:-2] +"]"
+        return "[]"
+
+
+node1 = Node(1)
+node2 = Node(2)
+node3 = Node(3)
+node4 = Node(4)
+node5 = Node(5)
+
+my_list = LinkedList()
+
+my_list.append_val(node1)
+my_list.append_val(node2)
+my_list.append_val(node3)
+my_list.append_val(node4)
+my_list.append_val(node5)
+print(my_list)
