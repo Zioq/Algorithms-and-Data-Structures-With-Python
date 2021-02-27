0a48dd36c4c027056c4d5cba1ddbc8f098471435
Zioq <jr.roberthan@gmail.com>
2021-02-27 13:01:33 -0800

    update recursive reverse linked list

--- 18.Linked Lists/reversed_linked_list_recur.py
+++ 18.Linked Lists/reversed_linked_list_recur.py
@@ -38,7 +38,23 @@ class LinkedList:
         ''' reverse the sequence of node pointers in the linked list '''
         # Given [1->2->3->4->5] reverse pointers [1<-2<-3<-4<-5]
         # Turning list to [5->4->3->2->1]
-        pass
+        
+        # If list has nothing, return nothing(Because there is nothing to reverse)
+        if self.head == None:
+            return
+
+        # Best Case(When we reached end of our linked list having reversed a pointer)
+        # Reached the tail and change that tail's pointer points previous element, not points 'None'
+        elif current.next == None: # Reach the tail
+            self.tail = self.head # Set the tail as previous head ! (Set the `5`(which was tail) as `Head`(which is previous head))
+            current.next = previous # Set the current node point previous node
+            self.head = current # Set the current as 'New Head'
+        
+        # iterate through the list 
+        else:  
+            next = current.next
+            current.next = previous
+            self.reverse_list_recur(next,current)
 
 
 # Test
@@ -48,4 +64,6 @@ my_list.add_val(2)
 my_list.add_val(3)
 my_list.add_val(4)
 my_list.add_val(5)
+print(my_list)
+my_list.reverse_list_recur(my_list.head, None)
 print(my_list)
