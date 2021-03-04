385f7fa715bd145fd06cb5259a8c969e921d391d
Zioq <jr.roberthan@gmail.com>
2021-03-04 13:24:16 -0800

    add new comment

--- 21.Binary Search Tree/bst_delete_1_child_node.py
+++ 21.Binary Search Tree/bst_delete_1_child_node.py
@@ -49,6 +49,7 @@ class BSTDemo:
             if key == curr.data:
                 if curr.left_child and curr.right_child:
                     print("problem scenario")
+                # Set the condition when there is no children.
                 if curr.left_child == None and curr.right_child == None:
                     if is_left:
                         prev.left_child = None
